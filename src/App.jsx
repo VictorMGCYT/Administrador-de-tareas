2b8351b07763215165ed2585567d0b7d49f17b8f
VictorMGCYT <manuelvgc2233@gmail.com>
2025-01-25 11:28:32 -0600

    Funcion borrar tareas
    25.01.25

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,73 @@ import { useState, useEffect } from 'react'
 import './App.scss'
 import Header from './components/Header'
 import TaskList from './components/TaskList'
+import Swal from 'sweetalert2'
 
 function App() {
 
-  const [task, setTask] = useState([]);
-  const [textTask, setTextTask] = useState('');
+    const [task, setTask] = useState([]);
+    const [textTask, setTextTask] = useState('');
 
-  function saveTask(tarea) {
-    if (tarea === '') {
-      console.log('La tarea no puede estar vacía');
-    } else {
-      setTask([...task, tarea]);
-      console.log(`La tarea ${tarea} fue agregada correctamente`)
-      setTextTask('');
+    useEffect(()=>{
+        
+    },[task])
+
+    function saveTask(tarea) {
+        if (tarea === '') {
+        console.log('La tarea no puede estar vacía');
+        } else {
+        setTask([...task, tarea]);
+        console.log(`La tarea ${tarea} fue agregada correctamente`)
+        setTextTask('');
+        }
     }
-  }
+
+    function deleteTask(id) {
+        Swal.fire({
+            title: "¿Estás seguro?",
+            text: "¡No podrás revertir esto!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sí, borrarlo"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                const updatedTask = [...task];
+
+                updatedTask[id] = undefined;
+                console.log(updatedTask);
+
+                setTask(updatedTask);
+              Swal.fire({
+                title: "¡Borrado!",
+                text: "Tu tarea ha sido borrada.",
+                icon: "success"
+              });
+            }
+          });
+        
+
+    }
 
 
-  return (
-    <>
-      <Header
-        textTask={textTask}
-        setTextTask={setTextTask}
-        saveTask={saveTask}
-      >
+    return (
+        <>
+            <Header
+                textTask={textTask}
+                setTextTask={setTextTask}
+                saveTask={saveTask}
+            >
 
-      </Header>
-      <TaskList
-        task={task}
-      >
+            </Header>
+            <TaskList
+                task={task}
+                deleteTask={deleteTask}
+            >
 
-      </TaskList>
-    </>
-  )
+            </TaskList>
+        </>
+    )
 }
 
 export default App
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,19 +1,21 @@
 import '../components/TaskList.scss'
 
-function TaskList({task}) {
+function TaskList({task, deleteTask}) {
     
     return (
         <>
             <div className="TaskList">
                 {task.map((item, index) => {
 
-                    return (
-                        <div className='item-task'>
+                    
+                    return item !== null && item !== undefined ? (
+                        <div key={index} className='item-task'>
                             <input type="checkbox" name="" id="" />
                             <p key={index}>{item}</p>
-                            <input type="submit" value={`Borrar`} />
+                            <input onClick={() => deleteTask(index)} type="submit" value={`Borrar`} />
                         </div>
                     )
+                    : null
 
                 })}
             </div>
